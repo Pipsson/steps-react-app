f5c8f8c338619a77eedfaa68281bb9765e03c799
Pipsson <juliusmallya8420@gmail.com>
2024-10-14 04:09:57 +0300

    fixture: static part of app complited without state management.

---
+++ src/App.js
@@ -0,0 +1,51 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const step = 3;
+
+  function handlePrevious() {
+    alert("previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Steps{step} : {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        {/* styling individual cmp using object {{}} for jsx and then for styling */}
+
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#fff",
+          }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
